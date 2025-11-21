218e8f9b00d0b010c79d35de09c30bdbed25b2f1
Stephen <creativebensonglasgow@gmail.com>
2025-11-21 12:20:33 +0000

    Initial Umbraco Project Bootstrap and Clean Architecture DI Layering

---
+++ SupporterPortal.Web/Program.cs
@@ -0,0 +1,34 @@
+using SupporterPortal.Application;
+using SupporterPortal.Infrastructure;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.CreateUmbracoBuilder()
+    .AddBackOffice()
+    .AddWebsite()
+    .AddDeliveryApi()
+    .AddComposers()
+    .Build();
+
+builder.Services.AddApplication(builder.Configuration);
+builder.Services.AddInfrastructure(builder.Configuration);
+
+WebApplication app = builder.Build();
+
+await app.BootUmbracoAsync();
+
+
+app.UseUmbraco()
+    .WithMiddleware(u =>
+    {
+        u.UseBackOffice();
+        u.UseWebsite();
+    })
+    .WithEndpoints(u =>
+    {
+        u.UseInstallerEndpoints();
+        u.UseBackOfficeEndpoints();
+        u.UseWebsiteEndpoints();
+    });
+
+await app.RunAsync();
